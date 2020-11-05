a4346087c3ff484a3c250a715d5e23e6cbca1101
joyling93 <963034741@qq.com>
2020-11-05 17:53:41 +0800

    stat.sh add

--- stat.sh
+++ stat.sh
@@ -4,5 +4,5 @@ for sample in "$@"
 do
 	echo "${sample}:\n"
 	vcftools --gzvcf "${sample}" --depth --stdout
-	vcftools --gzvcf "${sample}" --TsTv --stdout
+	vcftools --gzvcf "${sample}" --TsTv-summary --stdout
 done
